745bb01f5a9938328a31c1faed424156e3d9f873
phunt <phunt@cloudera.com>
2010-05-15 00:27:56 -0700

    added HBase sink

--- src/java/com/cloudera/flume/conf/SinkFactoryImpl.java
+++ src/java/com/cloudera/flume/conf/SinkFactoryImpl.java
@@ -62,6 +62,7 @@ import com.cloudera.flume.handlers.debug.TextFileSink;
 import com.cloudera.flume.handlers.endtoend.AckChecksumChecker;
 import com.cloudera.flume.handlers.endtoend.AckChecksumInjector;
 import com.cloudera.flume.handlers.endtoend.ValueDecorator;
+import com.cloudera.flume.handlers.hbase.HBaseEventSink;
 import com.cloudera.flume.handlers.hdfs.CustomDfsSink;
 import com.cloudera.flume.handlers.hdfs.DFSEventSink;
 import com.cloudera.flume.handlers.hdfs.EscapedCustomDfsSink;
@@ -141,6 +142,9 @@ public class SinkFactoryImpl extends SinkFactory {
       { "multigrepspec", MultiGrepReporterSink.builder() },
       { "regexhisto", RegexGroupHistogramSink.builderSimple() },
       { "regexhistospec", RegexGroupHistogramSink.builder() },
+
+      // experiemental
+      { "hbase", HBaseEventSink.builder() },
 
       // deprecated
       { "thrift", ThriftEventSink.builder() },
---
+++ src/java/com/cloudera/flume/handlers/hbase/HBaseEventSink.java
@@ -0,0 +1,92 @@
+// This Cloudera, Inc. source code, including without limit any
+// human-readable computer programming code and associated documentation
+// (together "Source Code"), contains valuable confidential, proprietary
+// and trade secret information of Cloudera and is protected by the laws
+// of the U.S. and other countries. Any use of the Source Code, including
+// without limit any disclosure or reproduction, without the prior
+// written authorization of Cloudera is strictly prohibited.
+//
+// Copyright (c) 2010 Cloudera, Inc.  All rights reserved.
+package com.cloudera.flume.handlers.hbase;
+
+import java.io.IOException;
+import java.util.Map.Entry;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.cloudera.flume.conf.SinkFactory.SinkBuilder;
+import com.cloudera.flume.core.Event;
+import com.cloudera.flume.core.EventSink;
+import com.google.common.base.Preconditions;
+
+/**
+ * This generates an hbase output sink. Nanos is used as the row name,
+ * event timestamp, host, body and attributes are all added to the row.
+ */
+public class HBaseEventSink extends EventSink.Base {
+
+  String tableName;
+  String familyName;
+
+  HBaseConfiguration config;
+  HTable table;
+
+  public HBaseEventSink(String tableName, String familyName) {
+    Preconditions.checkNotNull(tableName);
+    Preconditions.checkNotNull(familyName);
+    this.tableName = tableName;
+    this.familyName = familyName;
+  }
+
+  @Override
+  public void append(Event e) throws IOException {
+    Put p = new Put(Bytes.toBytes(Long.toString(e.getNanos())));
+    
+    p.add(Bytes.toBytes(familyName), Bytes.toBytes("timestamp"),
+        Bytes.toBytes(e.getTimestamp()));
+    p.add(Bytes.toBytes(familyName), Bytes.toBytes("host"),
+        Bytes.toBytes(e.getHost()));
+    p.add(Bytes.toBytes(familyName), Bytes.toBytes("event"), e.getBody());
+
+    for (Entry<String, byte[]> a : e.getAttrs().entrySet()) {
+      p.add(Bytes.toBytes(familyName), Bytes.toBytes(a.getKey()), a.getValue());
+    }
+
+    table.put(p);
+  }
+
+  @Override
+  public void close() throws IOException {
+    table.close();
+  }
+
+  @Override
+  public void open() throws IOException {
+    // You need a configuration object to tell the client where to connect.
+    // When you create a HBaseConfiguration, it reads in whatever you've set
+    // into your hbase-site.xml and in hbase-default.xml, as long as these can
+    // be found on the CLASSPATH
+    config = new HBaseConfiguration();
+
+    // This instantiates an HTable object that connects you to
+    // the tableName table.
+    table = new HTable(config, tableName);
+  }
+
+  public static SinkBuilder builder() {
+    return new SinkBuilder() {
+
+      @Override
+      public EventSink build(String... argv) {
+        Preconditions.checkArgument(argv.length == 2,
+            "usage: hbase(table, family)");
+
+        return new HBaseEventSink(argv[0], argv[1]);
+      }
+
+    };
+  }
+}
