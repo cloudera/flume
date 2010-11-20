6e8022a03b747b1cb007658cecf321f0b92fab8a
Jonathan Hsieh <jon@cloudera.com>
2010-11-20 08:42:20 -0800

    FLUME-589: Improve Cancellation to take advantage of InterruptedException - tests for interruption handling in backoff failover sinks - tests for interruption handling in insistent open decorator - tests for interruption handling in insistent append decorator - tests for interruption handling in stubborn append sink

--- src/javatest/com/cloudera/flume/handlers/debug/TestInsistentAppend.java
+++ src/javatest/com/cloudera/flume/handlers/debug/TestInsistentAppend.java
@@ -20,6 +20,7 @@ package com.cloudera.flume.handlers.debug;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
@@ -31,34 +32,63 @@ import org.slf4j.LoggerFactory;
 import com.cloudera.flume.conf.FlumeBuilder;
 import com.cloudera.flume.conf.FlumeSpecException;
 import com.cloudera.flume.conf.ReportTestingContext;
+import com.cloudera.flume.core.EventImpl;
 import com.cloudera.flume.core.EventSink;
+import com.cloudera.flume.core.InterruptSinks;
 import com.cloudera.flume.reporter.ReportEvent;
 import com.cloudera.flume.reporter.ReportTestUtils;
 import com.cloudera.flume.reporter.ReportUtil;
+import com.cloudera.util.BackoffPolicy;
+import com.cloudera.util.CappedExponentialBackoff;
 
+/**
+ * This tests the insistent opener (it tries many times before giving up)
+ */
 public class TestInsistentAppend {
-  public static final Logger LOG = LoggerFactory
-      .getLogger(TestInsistentAppend.class);
+	public static final Logger LOG = LoggerFactory
+			.getLogger(TestInsistentAppend.class);
 
-  /**
-   * Test insistent append metrics
-   */
-  @Test
-  public void testInsistentAppendMetrics() throws JSONException,
-      FlumeSpecException, IOException, InterruptedException {
-    ReportTestUtils.setupSinkFactory();
+	/**
+	 * Test insistent append metrics
+	 */
+	@Test
+	public void testInsistentAppendMetrics() throws JSONException,
+			FlumeSpecException, IOException, InterruptedException {
+		ReportTestUtils.setupSinkFactory();
 
-    EventSink snk = FlumeBuilder.buildSink(new ReportTestingContext(),
-        "insistentAppend one");
-    ReportEvent rpt = ReportUtil.getFlattenedReport(snk);
-    LOG.info(ReportUtil.toJSONObject(rpt).toString());
-    assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_ATTEMPTS));
-    assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_GIVEUPS));
-    assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_REQUESTS));
-    assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_RETRIES));
-    assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_SUCCESSES));
-    assertNotNull(rpt.getStringMetric("backoffPolicy.CappedExpBackoff.name"));
-    assertEquals("One", rpt.getStringMetric("One.name"));
-  }
+		EventSink snk = FlumeBuilder.buildSink(new ReportTestingContext(),
+				"insistentAppend one");
+		ReportEvent rpt = ReportUtil.getFlattenedReport(snk);
+		LOG.info(ReportUtil.toJSONObject(rpt).toString());
+		assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_ATTEMPTS));
+		assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_GIVEUPS));
+		assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_REQUESTS));
+		assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_RETRIES));
+		assertNotNull(rpt.getLongMetric(InsistentAppendDecorator.A_SUCCESSES));
+		assertNotNull(rpt
+				.getStringMetric("backoffPolicy.CappedExpBackoff.name"));
+		assertEquals("One", rpt.getStringMetric("One.name"));
+	}
 
+	/**
+	 * Enforce the semantics of interruption exception handling.
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testAppendInterruption() throws IOException,
+			InterruptedException {
+		BackoffPolicy bop = new CappedExponentialBackoff(10, 5000);
+		EventSink snk = new InsistentAppendDecorator<EventSink>(
+				InterruptSinks.appendSink(), bop);
+		snk.open();
+		try {
+			snk.append(new EventImpl("test".getBytes()));
+		} catch (InterruptedException ie) {
+			// if interrupted, it should be closed.
+			return;
+		}
+		fail("expected interruption!");
+	}
 }
